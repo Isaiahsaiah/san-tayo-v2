989fb393f95f91441cc5a0263688688320b029a4
Isaiah <idpanoga@ecarga.com.ph>
2024-10-10 15:44:55 +0800

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  googleMapsApiKey: 'AIzaSyBcPDXuFSiAD4zabiRF_vUcNHnC-f06aow',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  googleMapsApiKey: 'AIzaSyBcPDXuFSiAD4zabiRF_vUcNHnC-f06aow',
+};
+
+// import { environment } from '../environments/environment';
+// const apiKey = environment.googleMapsApiKey;
